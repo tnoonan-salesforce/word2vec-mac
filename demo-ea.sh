e626d54c9a359261593db992ce1a4320debf560f
Tim <tnoonan@salesforce.com>
2020-09-03 11:39:49 -0600

    add corpus file option.

--- demo-ea.sh
+++ demo-ea.sh
@@ -1,2 +1,6 @@
+if [ ! -f $1 ]; then
+	echo "$1 is not found."
+	exit 1
+fi
 time ./word2vec -train ea_corpus.txt -output ea_vectors.bin -cbow 0 -size 300 -window 1 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
 ./distance ea_vectors.bin
--- demo-phrases-ea.sh
+++ demo-phrases-ea.sh
@@ -1,3 +1,8 @@
-time ./word2phrase -train ./ea_corpus.txt -output ea-phrase -threshold 500 -debug 2
+if [ ! -f $1 ]; then
+	echo "$1 is not found."
+	exit 1
+
+fi
+time ./word2phrase -train $1 -output ea-phrase -threshold 500 -debug 2
 time ./word2vec -train ea-phrase -output vectors-phrase-ea.bin -cbow 0 -size 300 -window 1 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
 ./distance vectors-phrase-ea.bin
