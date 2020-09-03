8c4b854a2ebed192cde1775621f97d433d446be2
Tim <tnoonan@salesforce.com>
2020-09-03 11:05:24 -0600

    update demo

---
+++ demo-ea.sh
@@ -0,0 +1,2 @@
+time ./word2vec -train ea_corpus.txt -output ea_vectors.bin -cbow 0 -size 300 -window 1 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
+./distance ea_vectors.bin
---
+++ demo-phrases-ea.sh
@@ -0,0 +1,3 @@
+time ./word2phrase -train ./ea_corpus.txt -output ea-phrase -threshold 500 -debug 2
+time ./word2vec -train ea-phrase -output vectors-phrase-ea.bin -cbow 0 -size 300 -window 1 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
+./distance vectors-phrase-ea.bin
